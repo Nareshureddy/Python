b9945faa81880adcf32338900fa93fc389ea1132
Nareshureddy <45947674+Nareshureddy@users.noreply.github.com>
2020-10-22 15:50:38 +0100

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,17 +1,17 @@
 print ("New Traingale")
-for i in range(4):
-	for j in range(4):
+for i in range(5):
+	for j in range(5):
 		print("# ",end="")
 	print()
 
 print ("New Traingale")
-for i in range(4):
+for i in range(5):
 	for j in range(i+1):
 		print("# ",end="")
 	print()
 
 print ("New Traingale")
-for i in range(4):
-	for j in range(4-i):
+for i in range(5):
+	for j in range(5-i):
 		print("# ",end="")
 	print()
